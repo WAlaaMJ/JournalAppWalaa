0d52171c52a8b2f4ec477d7959120e1cd7e3efbf
SumayahAbdullahQ <sumayahabdullah27@gmail.com>
2024-10-20 14:38:28 +0300

    Initial Commit

---
+++ JournalApp/JournalAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  JournalAppApp.swift
+//  JournalApp
+//
+//  Created by Sumayah Alqahtani on 17/04/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct JournalAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
